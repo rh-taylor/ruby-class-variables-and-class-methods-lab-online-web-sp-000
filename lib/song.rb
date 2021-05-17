48ca3d2a9404a53722cedacff80fa55872009a88
Robyn Taylor <rhtaylor0715@gmail.com>
2021-05-17 16:00:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,13 +45,3 @@ class Song
     end
     artist_count
 end
-
-
-  def self.artist_count
-    artist_count = Hash.new(0)
-    @@artists.each {|artist| artist_count[artist] += 1}
-    artist_count
-
-
-
-
