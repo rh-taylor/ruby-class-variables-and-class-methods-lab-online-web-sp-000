91c603b6eb09454dad58a9b38873abda010d5331
Robyn Taylor <rhtaylor0715@gmail.com>
2021-05-17 16:00:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   def self.genre_count
     genre_count = Hash.new(0)
     @@genres.each do |genre|
-      genre_count[genre] += 1
+      genre_count[genre] +=1
     end
     genre_count
   end
@@ -41,12 +41,16 @@ class Song
   def self.artist_count
     artist_count = Hash.new(0)
     @@artists.each do |artist|
-      
-  end
+      artist_count[artist] +=1
+    end
+    artist_count
 end
 
 
-
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each {|artist| artist_count[artist] += 1}
+    artist_count
 
 
 
